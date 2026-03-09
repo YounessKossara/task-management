27b9712cb42cdb9b74b90525991c7b06cfd26430
Youness <younesskossara@gmail.com>
2026-03-09 12:51:58 +0000

    Fix: update production URLs to Oracle IP

--- task-manager-frontend/src/environments/environment.ts
+++ task-manager-frontend/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://localhost:8080/api/v1',
-    keycloak: {
-        url: 'http://localhost:8180',
-        realm: 'task-manager',
-        clientId: 'task-manager-front'
-    }
+  production: true,
+  apiUrl: 'http://84.8.223.8:8080/api/v1',
+  keycloak: {
+    url: 'http://84.8.223.8:8180',
+    realm: 'task-manager',
+    clientId: 'task-manager-front'
+  }
 };
